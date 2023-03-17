304b1700fce5cb8ea3c461f4d664973775fbe5b8
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2023-03-17 22:24:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def get_music(pat, prompt, track_duration, gen_intensity, gen_mode):
     })
 
     rdata = json.loads(r.text)
-    print(rdata)
+    print(rdata['data'])
     #assert rdata['status'] == 1, "probably incorrect e-mail"
     #track = rdata['data']['tasks']['download_link']
 
